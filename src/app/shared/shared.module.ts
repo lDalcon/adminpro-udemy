854656fccce2b09bc49d9d86e67af17edd671b1a
Damian Duarte <damianleo1991@hotmail.com>
2018-11-04 22:45:42 -0500

    Agregamos y modificamos modulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+
+// ==============================================================
+// Components
+// ==============================================================
+
+import { BreadcrumsComponent } from './breadcrums/breadcrums.component';
+import { HeaderComponent } from './header/header.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+// ==============================================================
+// End Components
+// ==============================================================
+
+@NgModule({
+    declarations: [
+        BreadcrumsComponent,
+        HeaderComponent,
+        NopagefoundComponent,
+        SidebarComponent
+    ],
+    exports: [
+        BreadcrumsComponent,
+        HeaderComponent,
+        NopagefoundComponent,
+        SidebarComponent
+    ]
+})
+
+export class SharedModule {}
