be40b27d9bc732cb1db0baa47428508ddb71ec19
Damian Duarte <damianleo1991@hotmail.com>
2018-11-04 22:43:26 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,42 @@
+// ==========================================================
+// Routes
+// ==========================================================
+import { APP_ROUTES } from './app.routes';
+// ==========================================================
+// End Routes
+// ==========================================================
+// ==========================================================
+// Modules
+// ==========================================================
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-// Rutas
-
-import { APP_ROUTES } from './app.routes';
-
-// Componentea
-
+import { PagesModule } from './pages/pages.module';
+// ==========================================================
+// End Modules
+// ==========================================================
+// ==========================================================
+// Components
+// ==========================================================
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
-
+// ==========================================================
+// End Components
+// ==========================================================
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
